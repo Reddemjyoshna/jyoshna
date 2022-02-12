0d574c0f61a631f1bbb4b72657e138a661e69987
Reddemjyoshna <62939217+Reddemjyoshna@users.noreply.github.com>
2022-02-12 16:37:27 +0530

    Add files via upload

---
+++ strings/arr.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+class arr
+{
+	public static void main(String[] args) {
+		ArrayList<String> l=new ArrayList<String>();
+		l.add("jyo");
+		l.add("jyoshna");
+		System.out.println(l);
+	}
+}
